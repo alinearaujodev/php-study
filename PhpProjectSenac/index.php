7abbf1531f3dfb4995b252804b4b2bd7baaadec7
Aline de Araújo <alineprofissional@outlook.com>
2020-08-24 18:13:27 -0300

    add estrutura de controle

--- PhpProjectSenac/index.php
+++ PhpProjectSenac/index.php
@@ -14,16 +14,46 @@
         <?php
         echo "Hello World!</br>";
         
-        // Variáveis
+        // VARIAVEIS
         $nome = "Aline de Araújo";
         $num = 10;
         $num2 = 10;
         $total = $num + $num2;
         
-        // Apresentar variáveis com concatenação
+        // APRESENTAR VARIAVEIS COM CONCATENAÇÃO
         echo "Meu nome é " . $nome;
         echo "</br>";
         echo "E minha idade é " . $total . " anos";
+
+        // ESTRUTURA DE CONTROLE IF/ELSE
+        if ($num > 0){
+            echo "</br>$num é um numero positivo!";
+        } 
+        elseif($num < 0) {
+            echo "</br>$num é um numero negativo!"; 
+        }
+
+        // ESTRUTURA DE CONTROLE SWITCH
+        $op = "+";
+        $n1 = 10;
+        $n2 = 5;
+
+        switch ($op) {
+            case "+" : 
+                echo "</br> Soma = " . ($n1 + $n2);
+            break;
+            case "-":
+                echo "</br> Subtração = " . ($n1 - $n2);
+            break;
+            case "*":
+                echo "</br> Multiplicação = " . ($n1 * $n2);
+            break;
+            case "/":
+                echo "</br> Divisão = " . ($n1 / $n2);
+            break;
+            default:
+                echo "Comando invalido!";
+        }
         
         ?>
     </body>
