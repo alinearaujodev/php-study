aca629cc44e1f0330dbf8d3f0b850fe5817e89f1
Aline Araújo <alineprofissional@outlook.com>
2020-10-22 19:08:10 -0300

    estudado estrutura for no php

---
+++ php/teoria/07-for.php
@@ -0,0 +1,17 @@
+<?php
+    for($c=1; $c<=10; $c++){
+        echo "$c ";
+    }
+    echo "</br>";
+    for ($c=10; $c>=1; $c--){
+        echo "$c ";
+    }
+    echo "</br>";
+    for ($c=2; $c<=10; $c+=2){
+        echo "$c ";
+    }
+    echo "</br>";
+    for ($c=1; $c<=10; $c+=2){
+        echo "$c ";
+    }
+?>
